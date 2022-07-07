0a95dfbc8aed839ad86779554c15c0edab92f05b
Rlin232 <rlin@mitre.org>
2022-07-07 11:46:28 -0700

    Added mitre-saf formula
    Signed-off-by: Rlin232 <rlin@mitre.org>

---
+++ Formula/mitre-saf.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class MitreSaf < Formula
+  desc "CLI for streamlining security automation of systems and DevOps pipelines"
+  homepage "https://github.com/mitre/saf"
+  url "https://registry.npmjs.org/@mitre/saf/-/saf-1.1.11.tgz"
+  sha256 "d93b47b083835db60c3c2473aa50e6a71322efb45a76083c2f0a3e5452d0287e"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"saf", "--help"
+    assert_match "@mitre/saf/#{version}", shell_output("#{bin}/saf --version")
+    assert_match "Visit https://saf.mitre.org/#/validate", shell_output("#{bin}/saf --scan")
+  end
+end
